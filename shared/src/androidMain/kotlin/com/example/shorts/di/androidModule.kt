8491b44cc7ac3d98a521abb95cca551001139a4b
Ankit Singh2 <ankit.singh2@timesgroup.com>
2025-04-19 18:07:26 +0530

    koin injection

--- shared/src/androidMain/kotlin/com/example/shorts/di/androidModule.kt
+++ shared/src/androidMain/kotlin/com/example/shorts/di/androidModule.kt
@@ -1,11 +1,13 @@
 package com.example.shorts.di
 
+import com.example.shorts.articles.ArticlesUseCase
 import com.example.shorts.articles.ArticlesViewModel
 import com.example.shorts.database.DatabaseDriverFactory
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val androidModule = module {
+    single { ArticlesUseCase(get()) }
     single { DatabaseDriverFactory(get()) }
-    viewModel { ArticlesViewModel(get()) }
+    viewModel { ArticlesViewModel(get(),get()) }
 }
--- shared/src/commonMain/kotlin/com/example/shorts/di/sharedModule.kt
+++ shared/src/commonMain/kotlin/com/example/shorts/di/sharedModule.kt
@@ -1,11 +1,29 @@
 package com.example.shorts.di
 
 import com.example.shorts.DatabaseHelper
+import com.example.shorts.articles.ArticlesService
 import com.example.shorts.articles.ArticlesViewModel
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 import org.koin.dsl.module
 
 val sharedModule = module {
+
+    single {
+        HttpClient {
+            install(ContentNegotiation) {
+                json(Json {
+                    prettyPrint = true
+                    isLenient = true
+                    ignoreUnknownKeys = true
+                })
+            }
+        }
+    }
+    single { ArticlesService(get()) }
     single { DatabaseHelper(get()) }
-    single { ArticlesViewModel(get()) }
+    single { ArticlesViewModel(get(), get()) }
 }
 
