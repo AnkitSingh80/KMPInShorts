b8f2c4f963860aab07ac70753e0263d9240d4c38
Ankit Singh2 <ankit.singh2@timesgroup.com>
2025-05-27 17:30:09 +0530

    jvm dependencies updated

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -75,6 +75,7 @@ kotlin {
 
         val jvmMain by getting {
             dependencies {
+                implementation(libs.kotlinx.coroutines.core)
 
             }
         }
--- shared/src/jvmMain/kotlin/com/example/shorts/viewmodel/BaseViewModel.jvm.kt
+++ shared/src/jvmMain/kotlin/com/example/shorts/viewmodel/BaseViewModel.jvm.kt
@@ -1,6 +1,8 @@
 package com.example.shorts.viewmodel
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
 
 
